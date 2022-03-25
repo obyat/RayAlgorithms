63ebe2a1eb2854ce61f564ee64b2ac0cecbafb9b
Ray <rahman.obyat@gmail.com>
2022-03-24 23:07:43 -0400

    Add exercise question 10

---
+++ C6Q10.java
@@ -0,0 +1,13 @@
+public class C6Q10 {
+    public static void main(String args[]) {
+      System.out.println(isPrime(33));
+    }
+    
+    public static boolean isPrime(int n){
+         System.out.println("is " + n + " prime?  " );
+        for (int x = 2; x * x <= n; x++){
+            if (n % x  == 0) return false;
+        }
+        return true;
+    }
+}
