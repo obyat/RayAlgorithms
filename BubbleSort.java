fccb9c83d0f703690d6c04e87de60331eabcd431
Ray <rahman.obyat@gmail.com>
2022-03-28 17:21:55 -0400

    Add BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void sort(int[] array){
+        for(int i = 0; i<array.length; i++)
+            for(int j = 1; j<array.length - i; j++)
+                if(array[j]<array[j-1])
+                    swapArrayVals(array, j);
+    }
+    private static void swapArrayVals(int[] array, int j){
+        int temp = array[j];
+        array[j] = array[j-1];
+        array[j-1] = temp;
+    }
+    public static void main(String[] names){
+        int [] numbers = {};
+         BubbleSort.sort(numbers);
+         System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ C6E12.java
@@ -0,0 +1,28 @@
+public class C6E12 {
+    private static int count = 0;
+    public static void main(String args[]) {
+      permutation("abbc", "");
+    }
+
+    public void permutation(String str){
+        permutation(str, "");
+    }
+    
+    public static void permutation(String str, String prefix){
+
+        // System.out.println("the string to permute is: " + str + ", Prefix is:" + prefix);
+        if(prefix.length() == 4){
+            System.out.println(prefix);
+            count++;
+            System.out.println(count);
+        }
+        if(str.length() == 0){
+            // System.out.println(prefix);
+        } else {
+            for (int i=0; i < str.length(); i++){
+                String rem = str.substring(0, i) + str.substring(i+1);
+                permutation(rem, prefix + str.charAt(i));
+            }
+        }
+}
+}
