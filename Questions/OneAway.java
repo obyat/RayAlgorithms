5216917ebc222043b19be6a65edc8581e4b2af74
Ray <rahman.obyat@gmail.com>
2022-05-10 18:29:32 -0400

    Added solution for OneAway

--- Questions/OneAway.java
+++ Questions/OneAway.java
@@ -6,14 +6,31 @@ public class OneAway{
         remove a character, or replace a character. given two strings, write a function to 
         check if they are one edit (or zero edits) away.
     */
+
+    //runtime = O(Str1 + Str2 + 26)
     public static boolean oneAway(String str1, String str2){
         if (abs(str1.length() - str2.length()) > 1) return false;
-        //todo, take str1.toCharArray(), then i - 'a', then array[i]++
-        // str2.toCharArray(), if > 0 then array[i]--, otherwise array[i]++
-        // loop through char count array and add everything, if 2,1,0 then true
-        return true;
-    }
 
+        int[] charCount = new int[25];
+        int opCount = 0;
+
+        for(int i=0; i < str1.length(); i++)
+            charCount[str1.charAt(i) - 'a']++;
+
+        for(int i=0; i < str2.length(); i++){
+            if(charCount[str2.charAt(i) - 'a'] > 0)
+                charCount[str2.charAt(i) - 'a']--;
+            else
+                charCount[str2.charAt(i) - 'a']++;
+            }
+
+        for(int i=0; i < charCount.length; i++)
+            opCount += charCount[i];
+
+        if(opCount >= 0 && opCount <= 2)
+            return true;
+        return false;
+    }
     public static void assertTest(Object input, Object args, Object expected){
         if(!input.equals(expected)) {
             System.out.println("[Failed] expected: " + expected + ", received:" + input +  ", for: " + args);
@@ -34,12 +51,8 @@ public class OneAway{
         assertTest(oneAway("pale", "bake"), "pale, bake", false);
         assertTest(oneAway("pale", "balke"), "pale, balke", false);
         assertTest(oneAway("a", "abb"), "a, abb", false);
-        // System.out.println(0 - 1);
-
     }
 }
-
-
 
  
 /*
@@ -52,6 +65,7 @@ public class OneAway{
         2. sort both strings, and increament pointer, then substring all equal characters out, then compare the number
         3. convert string to ascii and increment count of each character, then decrement for second string, 
         4. index first string to an array with counts, in the second string if the value is >0 then subtract, otherwise add. Then go through the array and add the values, if it is 0,1,2, then true, else, false
+        5. Put all Characters in hashtable, then check if all chars in Str2 are contained in hashTable and keep count if not, then check count is 0 or 1
 
     Learned: Different solution, ideas:
         1.
