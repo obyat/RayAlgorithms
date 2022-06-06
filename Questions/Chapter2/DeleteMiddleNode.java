229004dc0881d1348bf26b18aff7815b5afd6db0
Ray <rahman.obyat@gmail.com>
2022-06-06 19:21:05 -0400

    Completed DeleteNode

--- Questions/Chapter2/DeleteMiddleNode.java
+++ Questions/Chapter2/DeleteMiddleNode.java
@@ -1,35 +1,96 @@
 package Chapter2;
 
+import java.util.HashSet;
+
+import Chapter2.LinkedLists.DllNode;
+import Chapter2.LinkedLists.DoublyLinkedList ;
 public class DeleteMiddleNode{
 
     /*
     Chapter: 2, Question: 3. 
-        Implement an algorithm to delete a node in the middle of a LinkedList, given only access to that node
+        Implement an algorithm to delete a any middle node of a LinkedList, given only access to that node
+        but not the first or last node
 
     Hints:
     */
-
-    //Runtime: 
-    public static boolean deleteMiddleNode(String str){
-        return true;
+    //Runtime: O(1) time, O(1) space
+    public static DoublyLinkedList deleteMiddleNode(DllNode node, DoublyLinkedList list){
+        if(node != null && node.next != null && node.prev != null){
+            node.prev.next = node.next;
+            node.next.prev = node.prev; //only correct if the list is doublylinked. The question did not specify the type of linkedlist
+        }
+        return list;
     }
+
+    public static DoublyLinkedList deleteMiddleNodeImproved(DllNode node, DoublyLinkedList list){
+        if(node != null && node.next != null && node.prev != null){
+            node.next = node.next.next;
+        }
+        return list;
+    }
+
+    public static DoublyLinkedList createLL(Integer[] arr){
+        if(arr.length <= 0) return null;
+        DllNode node = new DllNode(arr[0]);
+        DoublyLinkedList  list = new DoublyLinkedList (node);
+        for (int i = 1; i < arr.length; i++) list.addToEnd(arr[i]);
+        return list;
+    }
+
 
 
     public static void main(String[] args){ 
-        // deleteMiddleNode(isUniqueChars("abc"), "abc", true);
-        // System.out.println();
+        DoublyLinkedList l0 = createLL(new Integer[] {0});
+        DllNode l0Node = l0.head; // no removal
+        DoublyLinkedList l0Sol = createLL(new Integer[] {0});
 
+        DoublyLinkedList l1 = createLL(new Integer[] {0,1,2,3});
+        DllNode l1Node = l1.head; // remove 1
+        DoublyLinkedList l1Sol = createLL(new Integer[] {0,1,2,3});;
+
+        DoublyLinkedList l2 = createLL(new Integer[] {0,1,2,3,4,5,6});
+        DllNode l2Node = l2.head.next.next.next.next; // remove 4
+        DoublyLinkedList l2Sol = createLL(new Integer[] {0,1,2,3,5,6});
+
+        DoublyLinkedList l3 = createLL(new Integer[] {0,1,2,3,4,5,6});
+        DllNode l3Node = l3.head.next.next.next; //remove 3
+        DoublyLinkedList l3Sol = createLL(new Integer[] {0,1,2,4,5,6});
+
+        DoublyLinkedList l4 = createLL(new Integer[] {0,1,2,3,4,5,6});
+        DllNode l4Node = l4.head.next.next.next.next.next; // remove 5
+        DoublyLinkedList l4Sol = createLL(new Integer[] {0,1,2,3,4,6});
+
+
+        DoublyLinkedList l5 = createLL(new Integer[] {0,1,2,3,4,5,6});
+        DllNode l5Node = l5.head.next.next.next.next.next.next; //no removal
+        DoublyLinkedList l5Sol = createLL(new Integer[] {0,1,2,3,4,5,6});
+
+        assertTest(deleteMiddleNode(l0Node, l0).toString(), "l0", l0Sol.toString());
+        assertTest(deleteMiddleNode(l1Node, l1).toString(), "l1", l1Sol.toString());
+        assertTest(deleteMiddleNode(l2Node, l2).toString(), "l2", l2Sol.toString());
+        assertTest(deleteMiddleNode(l3Node, l3).toString(), "l3", l3Sol.toString());
+        assertTest(deleteMiddleNode(l4Node, l4).toString(), "l4", l4Sol.toString());
+        assertTest(deleteMiddleNode(l5Node, l5).toString(), "l5", l5Sol.toString());
+        System.out.println("\nImproved:\n");
+        assertTest(deleteMiddleNodeImproved(l0Node, l0).toString(), "l0", l0Sol.toString());
+        assertTest(deleteMiddleNodeImproved(l1Node, l1).toString(), "l1", l1Sol.toString());
+        assertTest(deleteMiddleNodeImproved(l2Node, l2).toString(), "l2", l2Sol.toString());
+        assertTest(deleteMiddleNodeImproved(l3Node, l3).toString(), "l3", l3Sol.toString());
+        assertTest(deleteMiddleNodeImproved(l4Node, l4).toString(), "l4", l4Sol.toString());
+        assertTest(deleteMiddleNodeImproved(l5Node, l5).toString(), "l5", l5Sol.toString());
     }
 
     /*
+    []-[a]-[z]
     Questions:
-        1.
+        1. Do we need to return anything?
+
 
     Ideas:
         1.
 
     Edge Cases:
-        1. 
+        1. Empty node
     
     Pros vs. Cons in solutons:
         1.
