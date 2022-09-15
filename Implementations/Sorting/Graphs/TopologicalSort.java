fa1b1d292468c5908bda331999581256c8c2a90d
Obyat Abdelrahman <rahman.obyat@gmail.com>
2022-09-15 19:07:57 -0400

    Removed redundant code for topologicalSort.java

--- Implementations/Sorting/Graphs/TopologicalSort.java
+++ Implementations/Sorting/Graphs/TopologicalSort.java
@@ -4,51 +4,56 @@ import java.util.List;
 import java.util.Stack;
 import java.util.ArrayList;
 
+//Runtime: Time: O(V+E). DFS with stack. Space: O(V).The extra space is needed for the stack. 
+
 public class TopologicalSort {
     private int VertexCount;
     private ArrayList<ArrayList<Integer>> adjList;
-    private List<Integer> res;
 
     private TopologicalSort(int vertexCount) {
         this.VertexCount = vertexCount;
         adjList = new ArrayList<ArrayList<Integer>>(vertexCount);
-        res = new ArrayList<>(vertexCount);
-        for (int i = 0; i < vertexCount; i++)
+        for (int i = 0; i < vertexCount; i++) {
             adjList.add(new ArrayList<Integer>());
-    }
-
-    public void addEdge(int vertex, int weight) {
-        adjList.get(vertex).add(weight);
-    }
-
-    public void sort(int vertex, boolean visited[], Stack<Integer> stack) {
-        visited[vertex] = true;
-
-        for (int currentVertex : adjList.get(vertex)) {
-            if (!visited[currentVertex])
-                sort(currentVertex, visited, stack);
         }
-        stack.push(vertex);
+    }
+
+    public void addEdge(int vertex, int neighbor) {
+        adjList.get(vertex).add(neighbor);
+        System.out.println("adjList: " + adjList.toString());
     }
 
     public List<Integer> sort() {
         Stack<Integer> stack = new Stack<Integer>();
         boolean visited[] = new boolean[VertexCount];
 
-        for (int i = 0; i < VertexCount; i++) {
+        for (int i = 0; i < VertexCount; i++)
             if (!visited[i])
                 sort(i, visited, stack);
+
+        System.out.println("Vertex " + stack.peek());
+
+        return stack;
+    }
+
+    public void sort(int vertex, boolean visited[], Stack<Integer> stack) {
+        visited[vertex] = true;
+        System.err.println("visiting vertex " + vertex);
+        System.out.println("adjList " + adjList.get(vertex));
+
+        for (int currentVertex : adjList.get(vertex)) {
+            System.out.println("curr " + currentVertex);
+            if (!visited[currentVertex])
+                sort(currentVertex, visited, stack);
         }
-
-        while (!stack.isEmpty())
-            res.add(stack.pop());
-
-        return res;
+        System.err.println("pushing vertex " + vertex);
+        stack.push(vertex);
     }
 
     public static void main(String[] args) {
-        TopologicalSort topSort = new TopologicalSort(6);
+        TopologicalSort topSort = new TopologicalSort(7);
         topSort.addEdge(5, 2);
+        topSort.addEdge(5, 6);
         topSort.addEdge(5, 0);
         topSort.addEdge(4, 0);
         topSort.addEdge(4, 1);
@@ -56,7 +61,7 @@ public class TopologicalSort {
         topSort.addEdge(3, 1);
 
         System.out.println("TopologicalSort: ");
-        System.out.println(topSort.sort().toString());
+        System.out.println(topSort.sort().toString()); // 5, 4, 2, 3, 1, 0
 
     }
 }
